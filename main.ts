5299da4cc75882d37ba4712c9c8060e654e8859e
Saad bin Abdulaziz "Chillingero" Mohammed Abdullah <160509127+chillinger692@users.noreply.github.com>
2024-02-20 09:52:34 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (rmessage1 == 0) {
-        rmessage1 = receivedNumber
-    } else if (rmessage2 == 0) {
-        rmessage2 = receivedNumber
-    } else if (rmessage3 == 0) {
-        rmessage3 = receivedNumber
-    } else if (rmessage4 == 0) {
-        rmessage4 = receivedNumber
-    } else if (rmessage5 == 0) {
-        rmessage5 = receivedNumber
-    } else if (rmessage6 == 0) {
-        rmessage6 = receivedNumber
-    } else {
-        basic.showString("Inbox full")
-    }
+    rmessage1 = receivedNumber
 })
 input.onButtonPressed(Button.A, function () {
     if (appno == 0) {
@@ -23,7 +9,7 @@ input.onButtonPressed(Button.A, function () {
             if (hourcalib == 24) {
                 hourcalib = 0
             }
-            basic.showString(time, 30)
+            basic.showString(time, 20)
         }
         if (appID % 7 == 6) {
             appID += 1
@@ -145,13 +131,7 @@ input.onButtonPressed(Button.A, function () {
         }
         if (appID == 61) {
             appID += 1
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                `)
+            basic.showString("Coming Soon", 40)
         }
         if (appID == 60) {
             appID += 1
@@ -196,9 +176,6 @@ input.onButtonPressed(Button.A, function () {
     if (appno == 42) {
         _7sstart = input.runningTime()
         basic.showIcon(IconNames.Chessboard)
-    }
-    if (appno == 41) {
-        basic.showString("Wait until Beta 0.11")
     }
     if (appno == 6) {
         if (appID == 83) {
@@ -246,13 +223,7 @@ input.onButtonPressed(Button.A, function () {
             appID = 70
         }
         if (appID == 71) {
-            basic.showLeds(`
-                # # # # #
-                # # . # #
-                # . # . #
-                # . . . #
-                # # # # #
-                `)
+            basic.showString("Only on V2")
             appID = 72
         }
         if (appID == 70) {
@@ -265,111 +236,105 @@ input.onButtonPressed(Button.A, function () {
                 `)
             appID = 71
         }
-    }
-    if (appno == 30) {
-        if (messagesymbol == 32) {
-            messagesymbol = 21
-        }
-        if (messagesymbol == 31) {
-            basic.showString("Cool")
-            messagesymbol = 31
-        }
-        if (messagesymbol == 30) {
-            basic.showString("Wow")
-            messagesymbol = 30
-        }
-        if (messagesymbol == 29) {
-            basic.showString("Hi")
-            messagesymbol = 29
-        }
-        if (messagesymbol == 28) {
-            basic.showString("Hello")
-            messagesymbol = 28
-        }
-        if (messagesymbol == 27) {
-            basic.showString("Zzz")
-            messagesymbol = 27
-        }
-        if (messagesymbol == 26) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # # # .
-                # . . . #
-                `)
-            messagesymbol = 26
-        }
-        if (messagesymbol == 25) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                . . . . .
-                . . . . .
-                # # # # #
-                `)
-            messagesymbol = 25
-        }
-        if (messagesymbol == 24) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # # # # #
-                # . . . #
-                # . . . #
-                `)
-            messagesymbol = 24
-        }
-        if (messagesymbol == 23) {
-            basic.showString("How are you")
-            messagesymbol = 23
-        }
-        if (messagesymbol == 22) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # . . . #
-                # . . . #
-                # # # # #
-                `)
-            messagesymbol = 22
-        }
-        if (messagesymbol == 21) {
-            basic.showLeds(`
-                . . . # #
-                . . . # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
+        if (appno == 3) {
             messagesymbol = 20
-        }
-    }
-    if (appno == 31) {
-        if (messageid == 4) {
-            messageid += -3
-        }
-        if (messageid == 3) {
-            basic.showNumber(3)
-            messageid += 1
-        }
-        if (messageid == 2) {
-            basic.showNumber(2)
-            messageid += 1
-        }
-        if (messageid == 1) {
-            basic.showNumber(1)
-            messageid += 1
+            if (messagesymbol == 32) {
+                messagesymbol = 20
+            }
+            if (messagesymbol == 31) {
+                basic.showString("Cool")
+                messagesymbol = 32
+            }
+            if (messagesymbol == 30) {
+                basic.showString("Wow")
+                messagesymbol = 31
+            }
+            if (messagesymbol == 29) {
+                basic.showString("Hi")
+                messagesymbol = 30
+            }
+            if (messagesymbol == 28) {
+                basic.showString("Hello")
+                messagesymbol = 29
+            }
+            if (messagesymbol == 27) {
+                basic.showString("Zzz")
+                messagesymbol = 28
+            }
+            if (messagesymbol == 26) {
+                basic.showLeds(`
+                    . . . . .
+                    . # . # .
+                    . . . . .
+                    . # # # .
+                    # . . . #
+                    `)
+                messagesymbol = 27
+            }
+            if (messagesymbol == 25) {
+                basic.showLeds(`
+                    . # . # .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # # # # #
+                    `)
+                messagesymbol = 26
+            }
+            if (messagesymbol == 24) {
+                basic.showLeds(`
+                    . # . # .
+                    . . . . .
+                    # # # # #
+                    # . . . #
+                    # . . . #
+                    `)
+                messagesymbol = 25
+            }
+            if (messagesymbol == 23) {
+                basic.showLeds(`
+                    . # . # .
+                    . . . . .
+                    # . . . #
+                    # . . . #
+                    # # # # #
+                    `)
+                messagesymbol = 24
+            }
+            if (messagesymbol == 22) {
+                basic.showString("How are you")
+                messagesymbol = 23
+            }
+            if (messagesymbol == 21) {
+                basic.showLeds(`
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    . . . # #
+                    . . . # #
+                    `)
+                messagesymbol = 22
+            }
+            if (messagesymbol == 20) {
+                basic.showLeds(`
+                    . . . # #
+                    . . . # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+                messagesymbol = 21
+            }
         }
     }
 })
 input.onButtonPressed(Button.AB, function () {
     if (appID == -100) {
-        basic.showString(time, 80)
-appID = 0
+        basic.showString(time)
+        appID = 0
     }
     if (appID == 0) {
-        basic.showString(time, 80)
+        basic.showString(time)
     }
     if (appno != 0) {
         appno = 0
@@ -383,53 +348,31 @@ input.onButtonPressed(Button.B, function () {
         if (minutecalib == 60) {
             minutecalib = 0
         }
-        basic.showString(time ,30)
+        basic.showString(time, 20)
     }
     if (appID == 0) {
         appID = -1
     }
     if (appID % 7 == 1) {
         appno = 1
-        appID = 51
-        basic.showLeds(`
-            # # . # #
-            # # # # #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
+        appID = 50
     }
     if (appID % 7 == 2) {
         appno = 2
     }
     if (appID % 7 == 3) {
         appno = 3
-        appID = 70
     }
     if (appID % 7 == 4) {
         appno = 4
     }
     if (appID % 7 == 5) {
         appno = 5
-        appID = 62
-        basic.showLeds(`
-            # # # # #
-            . . . # .
-            . . # . .
-            . # . . .
-            # . . . .
-            `)
+        appID = 60
     }
     if (appID % 7 == 0) {
         appno = 6
-        appID = 81
-        basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . # . #
-            # . . . #
-            # # # # #
-            `)
+        appID = 80
     }
     if (appno == 2) {
         basic.showString("" + walkingDistance + "/" + ("" + runningDistance))
@@ -484,6 +427,9 @@ input.onButtonPressed(Button.B, function () {
         _7sscore = 100 - Math.abs(_7stime - 7000)
         basic.showNumber(_7sscore)
     }
+    if (appno == 41) {
+        sprite = game.createSprite(2, 2)
+    }
     if (appID == 70) {
         appno = 30
     }
@@ -526,245 +472,40 @@ input.onButtonPressed(Button.B, function () {
     if (messagesymbol == 31) {
         radio.sendNumber(31)
     }
-    if (appno == 30) {
-        if (rmessage1 == 31) {
-            basic.showString("Cool")
-        }
-        if (rmessage1 == 30) {
-            basic.showString("Wow")
-        }
-        if (rmessage1 == 29) {
-            basic.showString("Hi")
-        }
-        if (rmessage1 == 28) {
-            basic.showString("Hello")
-        }
-        if (rmessage1 == 27) {
-            basic.showString("Zzz")
-        }
-        if (rmessage1 == 26) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # # # .
-                # . . . #
-                `)
-        }
-        if (rmessage1 == 25) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                . . . . .
-                . . . . .
-                # # # # #
-                `)
-        }
-        if (rmessage1 == 24) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # # # # #
-                # . . . #
-                # . . . #
-                `)
-        }
-        if (rmessage1 == 23) {
-            basic.showString("How are you")
-        }
-        if (rmessage1 == 22) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # . . . #
-                # . . . #
-                # # # # #
-                `)
-        }
-        if (rmessage1 == 21) {
-            basic.showLeds(`
-                . . . # #
-                . . . # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-        }
-    }
-    if (appno == 30) {
-        if (rmessage2 == 31) {
-            basic.showString("Cool")
-        }
-        if (rmessage2 == 30) {
-            basic.showString("Wow")
-        }
-        if (rmessage2 == 29) {
-            basic.showString("Hi")
-        }
-        if (rmessage2 == 28) {
-            basic.showString("Hello")
-        }
-        if (rmessage2 == 27) {
-            basic.showString("Zzz")
-        }
-        if (rmessage2 == 26) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # # # .
-                # . . . #
-                `)
-        }
-        if (rmessage2 == 25) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                . . . . .
-                . . . . .
-                # # # # #
-                `)
-        }
-        if (rmessage2 == 24) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # # # # #
-                # . . . #
-                # . . . #
-                `)
-        }
-        if (rmessage2 == 23) {
-            basic.showString("How are you")
-        }
-        if (rmessage2 == 22) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # . . . #
-                # . . . #
-                # # # # #
-                `)
-        }
-        if (rmessage2 == 21) {
-            basic.showLeds(`
-                . . . # #
-                . . . # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-        }
-    }
-    if (appno == 30) {
-        if (rmessage3 == 31) {
-            basic.showString("Cool")
-        }
-        if (rmessage3 == 30) {
-            basic.showString("Wow")
-        }
-        if (rmessage3 == 29) {
-            basic.showString("Hi")
-        }
-        if (rmessage3 == 28) {
-            basic.showString("Hello")
-        }
-        if (rmessage3 == 27) {
-            basic.showString("Zzz")
-        }
-        if (rmessage3 == 26) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # # # .
-                # . . . #
-                `)
-        }
-        if (rmessage3 == 25) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                . . . . .
-                . . . . .
-                # # # # #
-                `)
-        }
-        if (rmessage3 == 24) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # # # # #
-                # . . . #
-                # . . . #
-                `)
-        }
-        if (rmessage3 == 23) {
-            basic.showString("How are you")
-        }
-        if (rmessage3 == 22) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # . . . #
-                # . . . #
-                # # # # #
-                `)
-        }
-        if (rmessage3 == 21) {
-            basic.showLeds(`
-                . . . # #
-                . . . # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-        }
-    }
 })
 input.onGesture(Gesture.Shake, function () {
 	
 })
 let compplayedrps = 0
+let stopwatchmin = 0
+let sprite: game.LedSprite = null
 let _7sscore = 0
 let _7stime = 0
 let userplayedrps = 0
 let elapsed = 0
-let messageid = 0
 let messagesymbol = 0
 let _7sstart = 0
 let rps = 0
 let timerstart = 0
-let rmessage6 = 0
-let rmessage5 = 0
-let rmessage4 = 0
+let rmessage1 = 0
 let mincalibtimer = 0
 let appno = 0
 let stepcounter = 0
 let runningDistance = 0
 let walkingDistance = 0
 let appID = 0
-let rmessage3 = 0
-let rmessage2 = 0
-let rmessage1 = 0
 let minutecalib = 0
 let hourcalib = 0
-let stopwatchmin = 0
-let time = ""
-let walkingDistanceMiles = 0
-let runningDistanceMiles = 0
 let hourcalibtimer = 0
-let dotscore = 0
-let sprite = 0
-basic.showString("ChampCo SmartWatch Beta.", 75)
+let runningDistanceMiles = 0
+let walkingDistanceMiles = 0
+let time = ""
+basic.showString("Champ Smart Watch OS", 40)
 input.calibrateCompass()
 radio.setGroup(109)
-basic.showString("What is the time?", 75)
+basic.showString("What is the time?", 50)
 hourcalib = 0
 minutecalib = 0
-rmessage1 = 0
-rmessage2 = 0
-rmessage3 = 0
 appID = -100
 walkingDistance = 0
 runningDistance = 0
@@ -799,7 +540,7 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (appID == 50) {
-        basic.showString(time, 80)
+        basic.showString(time)
     }
 })
 basic.forever(function () {
@@ -808,23 +549,23 @@ basic.forever(function () {
 basic.forever(function () {
     if (appno == 4) {
         if (degrees > 0) {
-            basic.showString("" + degrees + ":" + "N", 80)
+            basic.showString("" + degrees + ":" + "N")
         } else if (degrees > 22) {
-            basic.showString("" + degrees + ":" + "NE", 80)
+            basic.showString("" + degrees + ":" + "NE")
         } else if (degrees > 67) {
-            basic.showString("" + degrees + ":" + "E", 80)
+            basic.showString("" + degrees + ":" + "E")
         } else if (degrees > 112) {
-            basic.showString("" + degrees + ":" + "SE", 80)
+            basic.showString("" + degrees + ":" + "SE")
         } else if (degrees > 157) {
-            basic.showString("" + degrees + ":" + "S", 80)
+            basic.showString("" + degrees + ":" + "S")
         } else if (degrees > 202) {
-            basic.showString("" + degrees + ":" + "SW", 80)
+            basic.showString("" + degrees + ":" + "SW")
         } else if (degrees > 247) {
-            basic.showString("" + degrees + ":" + "W", 80)
+            basic.showString("" + degrees + ":" + "W")
         } else if (degrees > 292) {
-            basic.showString("" + degrees + ":" + "NW" , 80)
+            basic.showString("" + degrees + ":" + "NW")
         } else {
-            basic.showString("" + degrees + ":" + "N" , 80)
+            basic.showString("" + degrees + ":" + "N")
         }
     }
 })
@@ -833,7 +574,7 @@ basic.forever(function () {
         basic.pause(60000)
         stopwatchmin += -1
     }
-    if (stopwatchmin = 1) {
+    if (stopwatchmin == 1) {
         basic.pause(60000)
         stopwatchmin += -1
         basic.showLeds(`
@@ -863,7 +604,7 @@ basic.forever(function () {
     if (appno == 40) {
         compplayedrps = randint(1, 3)
     }
-    if (compplayedrps == userplayedrps && rps) {
+    if (compplayedrps == userplayedrps && userplayedrps != 0) {
         basic.showString("Draw")
     }
     if (compplayedrps == 1 && userplayedrps == 2 || (compplayedrps == 2 && userplayedrps == 3 || compplayedrps == 3 && userplayedrps == 1)) {
